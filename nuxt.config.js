5396ca7c3168df6a3c71a131803fbb9eb8cf1cfd
ikasumiwt <wtsnyk13@gmail.com>
2018-10-21 15:40:07 +0900

    updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-      { hid: 'og-image', property: 'og-image', content: 'https://user-images.githubusercontent.com/4006425/47263876-6cd29580-d545-11e8-86a6-65b06f62f136.png' }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'og-image', property: 'og-image', content: 'og-image.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
